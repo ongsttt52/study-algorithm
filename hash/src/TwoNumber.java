155d0998f692fff2becbb6d9bfabf7de4f0c7327
ongsttt52 <ongsttt52@gmail.com>
2026-01-07 10:02:42 +0900

    260107 - 해시

---
+++ hash/src/TwoNumber.java
@@ -0,0 +1,19 @@
+/**
+ * p.236 두 개의 수로 특정값 만들기
+ * n개의 양의 정수로 이루어진 배열 arr과 정수 target이 주어졌을 때, 이 중에서 합이 target인 두 수가 arr에 있는지 찾고 있으면 true, 없으면 false를 반환하는 solution() 함수를 구현하시오.
+ *
+ * n은 2 이상, 10,000 이하의 자연수
+ * arr의 각 원소는 1 이상 10,000 이하의 자연수
+ * arr의 원소 중 중복되는 원소는 없음
+ * target은 1 이상 20,000 이하의 자연수
+ *
+ * 권장 시간 복잡도 O(N+K)
+ */
+public class TwoNumber {
+    public static boolean solution(int[] arr, int target) {
+        // 내 생각 : 10,000개 이하의 정수 덧셈 후 검색 -> 대충 10000^2 / 2 ?? 5억번 정도 연산 필요 -> 이 횟수를 줄이려면
+        //
+        return true;
+    }
+
+}
